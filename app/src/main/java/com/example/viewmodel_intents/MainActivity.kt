41eccf940133fadb077e0284a11c820d4cbb8c22
Dinithi Sanjana <IT22074072@my.sliit.lk>
2024-04-20 08:26:24 +0530

    First MainActivity Implementation (count var refreshing with the orientation)

--- app/src/main/java/com/example/viewmodel_intents/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel_intents/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.viewmodel_intents
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val textView:TextView = findViewById(R.id.textView)
+        val button:Button = findViewById(R.id.button)
+        textView.setText(count.toString())
+
+        button.setOnClickListener{
+            count++
+
+            textView.setText(count.toString())
+
+
+        }
+
     }
 }
